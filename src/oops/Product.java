8b7d2fde53c14a8728b871df96e452917e851997
sandeep bhingle <sandeepbhingle@gmail.com>
2022-02-07 10:59:20 +0530

    Class and Objects

--- src/oops/Product.java
+++ src/oops/Product.java
@@ -1,17 +1,23 @@
 package oops;
 
 public class Product {
-    int productId;
-    String productName;
-    float productPrice;
+    int productId = 1;
+    String productName = "Abc";
+    float productPrice = 12.4f;
+    static String companyName = "Cyber success";
 
     void registerProduct(){
         System.out.println("registering new product");
     }
 
     public static void main(String[] args) {
-        int a = 20;
-        Product product = new Product();
+        //args is also local reference variable
+        int a = 20;//local primitive var
+        Product product = new Product(); // local reference variable
+        System.out.println(product.productId);
+        System.out.println(product.productName);
+        System.out.println(product.productPrice);
+        System.out.println(Product.companyName);
     }
 
 }
